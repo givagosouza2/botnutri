5eb051b1f5fd0812e1a1f572236eee959ae579c1
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-07-29 16:09:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import streamlit as st
 from openai import OpenAI
 from openai import RateLimitError
 
-
 st.set_page_config(
     page_title="NutriBot",
     page_icon="😃",
     layout="wide"
 )
 
-# Configura a chave da API
-openai.api_key = st.secrets["openai"]["api_key"]
+# Instancia o cliente
+client = OpenAI(api_key=st.secrets["openai"]["api_key"])
 
 st.title("NutriBot")
 
@@ -18,12 +17,12 @@ user_text = st.text_input("Inserir texto")
 
 if st.button("📤 Enviar avaliação ao ChatGPT"):
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": user_text}]
         )
 
-        st.markdown(response['choices'][0]['message']['content'])
+        st.markdown(response.choices[0].message.content)
 
     except RateLimitError:
         st.error("❌ Você atingiu o limite de requisições. Tente novamente em instantes.")
